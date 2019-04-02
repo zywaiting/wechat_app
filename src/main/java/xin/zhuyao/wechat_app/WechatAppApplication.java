bde3748a7031e35cc33a63af04502b3225c0f51f
zy <1573240324@qq.com>
2019-04-02 18:54:28 +0800

    删除

--- src/main/java/xin/zhuyao/wechat_app/WechatAppApplication.java
+++ src/main/java/xin/zhuyao/wechat_app/WechatAppApplication.java
@@ -1,8 +1,12 @@
 package xin.zhuyao.wechat_app;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableApolloConfig
 @SpringBootApplication
 public class WechatAppApplication {
 
